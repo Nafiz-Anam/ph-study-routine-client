9f824b2c3a5dd800ec41fad4eb250c4472f8d391
Nafiz Anam <nafiz.anam002@gmail.com>
2024-03-12 19:14:51 +0600

    update server url

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,17 @@ const nextConfig = {
                 hostname: "localhost",
                 port: "5000",
             },
+            {
+                protocol: "https",
+                hostname: "ph-study-routine-server.onrender.com",
+                port: "",
+            },
         ],
     },
     env: {
-        NEXT_PUBLIC_SERVER_URL: "http://localhost:5000/api/v1",
+        NEXT_PUBLIC_SERVER_URL:
+            "https://ph-study-routine-server.onrender.com/api/v1",
+        // NEXT_PUBLIC_SERVER_URL: "http://localhost:5000/api/v1",
     },
 };
 
